4ae29a442ffc18b8238636b92fd9ab6cafa181ae
skhadge <suchit.khadge@gmail.com>
2022-09-06 10:58:53 -0700

    Added verifyInternetConnection

--- Lab02.2/src/Television.java
+++ Lab02.2/src/Television.java
@@ -1,14 +1,79 @@
-public class Television {
+public class Television{
 
-    String brand;
-    int volume;
+     String brand;
+     int volume;
+     String display_ON;
 
-    void turnOn(){
-        System.out.println("The" + brand + "television is on, with a volume of " + volume);
-      }
+  private boolean verifyInternetConnection() {
+    return true;
+  }
+
+  public String getBrand(){
+    return brand;
+  }
+
+  public int getVolume(){
+    return volume;
+  }
+
+  public void setVolume(int volume) {
+    this.volume = volume;
+  }
+
+  public void setBrand(String brand) {
+    this.brand = brand;
+  }
+
+  public String toStringOn(){
+    return " The television brand " + getBrand() + ", is ON with a volume of " + getVolume();
+  }
+
+  public String toStringOff(){
+    return " The television brand " + getBrand() + ", is OFF";
+  }
+
+  public void printStream(String display_ON){
+    this.display_ON = display_ON;
+    System.out.println(display_ON);
+  }
+
+  public void Television( String brand_name, int volume_state){
+    brand = brand_name;
+    volume = volume_state;
+    toStringOn();
+    getVolume();
+    getBrand();
+    if (volume_state == 0) {
+      printStream(toStringOff());
+    }
+    else
+    {
+      printStream(toStringOn());
+    }
+
+
+
+
+    }
+
+    public void turnOn(){
+        boolean isConnected = verifyInternetConnection();
+        System.out.println("The"+brand+"television is on,with a volume of"+volume);
+    }
 
     void turnOff(){
-      System.out.println("The" + brand + "television is off ");
-      }
+        System.out.println("The"+brand+"television is OFF");
+    }
 
-}
+//    public void setBrand(String brand) {
+//        this.brand = brand;
+//    }
+//
+//    public void setVolume(int volume) {
+//        this.volume = volume;
+//    }
+
+
+
+
+    }
--- Lab02.2/src/TelevisionClient.java
+++ Lab02.2/src/TelevisionClient.java
@@ -1,17 +1,33 @@
-public class TelevisionClient {
 
-  public static void main(String[] args) {
-    Television tv1 = new Television();
-    tv1.brand = " TCL ";
-    tv1.volume = 50;
-    tv1.turnOn();
-    tv1.turnOff();
+    public class TelevisionClient{
+//
 
-    Television tv2 = new Television();
-    tv2.brand = " Samsung ";
-    tv2.volume = 75;
-    tv2.turnOff();
-    tv2.turnOn();
+    public static void main(String []args){
 
-  }
+//        Television tv1=  new Television();
+//    tv1.brand="TCL";
+//    tv1.volume=50;
+//    tv1.turnOn();
+//    tv1.turnOff();
+
+//        tv1.setBrand("TCL");
+//        tv1.setVolume(50);
+//        System.out.println(tv1.toStringOn());
+//        System.out.println(tv1.toStringOff());
+////
+//
+//        Television tv2=new Television();
+//        tv2.setBrand("Samsung");
+//        tv2.setVolume(100);
+//        System.out.println(tv2.toStringOn());
+//        System.out.println(tv2.toStringOff());
+
+        Television my_television= new Television();
+//        String brand = brand_name;
+//        int volume = volume_state;
+        my_television.Television("Toshiba", 75);
+        my_television.Television("Samsung", 100);
+        my_television.Television("TCL", 0);
+    }
+
 }
